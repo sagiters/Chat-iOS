b5f1f1b2e177dc88544769edd105e2e4bfaa0f4b
Jack <sagiters19@gmail.com>
2021-08-08 15:26:18 +0800

    import firebase

--- Chat/Chat/ChatApp.swift
+++ Chat/Chat/ChatApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ChatApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
