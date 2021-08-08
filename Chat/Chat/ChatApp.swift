c8bbed8515dbc011a85ee2dea76e2a3bbb26e185
Jack <sagiters19@gmail.com>
2021-08-08 17:01:29 +0800

    Complete is login or not

--- Chat/Chat/ChatApp.swift
+++ Chat/Chat/ChatApp.swift
@@ -17,7 +17,7 @@ struct ChatApp: App {
 
     var body: some Scene {
         WindowGroup {
-            LoginView().environmentObject(AuthViewModel())
+            ContentView().environmentObject(AuthViewModel())
         }
     }
 }
